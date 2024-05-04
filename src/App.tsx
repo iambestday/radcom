c87afb4d6c4c20e855c471a96600e3b695bde3ed
iambestday <behrouz.asghari@gmail.com>
2024-05-04 13:45:50 +0330

    Change texts

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       setArrayValues(newArrayValues);
       setError("")
     } else {
-      setError("ابعاد آرایه باید فرد و بزرگتر از 3 باشد");
+      setError("ابعاد  باید فرد و بزرگتر از 3 باشد");
     }
   };
 
